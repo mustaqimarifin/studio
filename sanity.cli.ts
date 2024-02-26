1026cd5711ee592135708ddcc4a26418f988c1ab
oddjobs <oddjobs@oddjobs.local>
2024-02-27 00:20:44 +0800

    YEAYEAYEA2

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '861c1zhj',
+    projectId: 'do33z8xq',
     dataset: 'production',
   },
 })
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,5 @@
 import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+import {structureTool} from 'sanity/structure'
 import {schemaTypes} from './schemas'
 import {markdownSchema} from 'sanity-plugin-markdown'
 import {visionTool} from '@sanity/vision'
@@ -7,9 +7,9 @@ export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05
 export default defineConfig({
   name: 'default',
   title: 'studioOlive',
-  projectId: '861c1zhj',
+  projectId: 'do33z8xq',
   dataset: 'production',
-  plugins: [deskTool(), markdownSchema(), visionTool({defaultApiVersion: apiVersion})],
+  plugins: [structureTool(), markdownSchema(), visionTool({defaultApiVersion: apiVersion})],
   schema: {
     types: schemaTypes,
   },
